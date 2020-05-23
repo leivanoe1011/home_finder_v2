a38d3a5241de1fb67ff8ba922e06df51868b1f8e
Noe Leiva <leiva.noe.86@gmail.com>
2020-05-23 10:50:45 -0500

    css to change the sidenav overlay to be transparent. In addition create state js api pull

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,10 @@
+
+var states = [];
+
 $(document).ready(function () {
 
+    stateApi();
+    
     $('.sidenav').sidenav();
 
     var zipCode = 37664;//$("#userZip").val().trim();
@@ -85,3 +90,30 @@ $(document).ready(function () {
         };
     });
 });
+
+function loadStates(data){
+    
+    console.log(data);
+    // states.push()
+}   
+
+
+function stateApi (){
+    
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://states2.p.rapidapi.com/query?country=USA%3Fcountry%3DUSA",
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "states2.p.rapidapi.com",
+            "x-rapidapi-key": "0c292c0993mshd75f0effe5adad9p120e45jsn157b0022e4d8",
+            "country": "USA"
+        }
+    }
+    
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+}
+
